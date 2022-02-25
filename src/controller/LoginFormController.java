d42720d9da6498f292a9830f47dc37ada9ef3bae
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-02-25 12:55:26 +0530

    Validate user inputs in the login form

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -2,9 +2,12 @@ package controller;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class LoginFormController {
 
@@ -16,11 +19,40 @@ public class LoginFormController {
     public Button btnExit;
 
     public void initialize() {
-        Platform.runLater(()->txtUserName.requestFocus());
+        Platform.runLater(() -> txtUserName.requestFocus());
     }
 
     public void btnConnect_OnAction(ActionEvent event) {
 
+        /* Let's validate some inputs */
+        if (txtHost.getText().trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Host can't be empty").show();
+            txtHost.requestFocus();
+            txtHost.selectAll();
+            return;
+        }else if (!txtPort.getText().matches("\\d+")){
+            new Alert(Alert.AlertType.ERROR, "Invalid port").show();
+            txtPort.requestFocus();
+            txtPort.selectAll();
+            return;
+        }else if (txtUserName.getText().trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Username can't be empty").show();
+            txtUserName.requestFocus();
+            txtUserName.selectAll();
+            return;
+        }
+
+        String command = String.format("mysql -h %s -u %s -p%s --port %s",
+                txtHost.getText(),
+                txtUserName.getText(),
+                txtPassword.getText(),
+                txtPort.getText());
+        try {
+            Process mysql = Runtime.getRuntime().exec(command);
+            System.out.println(mysql.waitFor());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void btnExit_OnAction(ActionEvent event) {
