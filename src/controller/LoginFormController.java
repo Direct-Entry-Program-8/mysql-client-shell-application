9470a49dde7937f3db20220824b0a0a7013639bd
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-02-25 12:44:52 +0530

    Finalize the UI design of Login Form

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,4 +1,30 @@
 package controller;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginFormController {
+
+    public TextField txtHost;
+    public TextField txtPort;
+    public TextField txtUserName;
+    public PasswordField txtPassword;
+    public Button btnConnect;
+    public Button btnExit;
+
+    public void initialize() {
+        Platform.runLater(()->txtUserName.requestFocus());
+    }
+
+    public void btnConnect_OnAction(ActionEvent event) {
+
+    }
+
+    public void btnExit_OnAction(ActionEvent event) {
+        System.exit(0);
+    }
+
 }
