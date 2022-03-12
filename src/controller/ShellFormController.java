a93c5e9b9cb4abefd06f81b8b2fb424061100a18
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-03-12 14:13:01 +0530

    Finalize mysql client

--- src/controller/ShellFormController.java
+++ src/controller/ShellFormController.java
@@ -1,34 +1,45 @@
 package controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.paint.Paint;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ShellFormController {
     public TextField txtCommand;
     public Button btnExecute;
     public TextArea txtOutput;
+    public Label lblCurrentSchema;
     private Process mysql;
 
-    public void initialize(){
+    public void initialize() {
+        txtCommand.setText("SHOW DATABASES;");
         txtOutput.setWrapText(true);
     }
 
     public void initData(String host, String port, String userName, String password) {
+        ProcessBuilder mysqlBuilder = new ProcessBuilder("mysql",
+                "-h", host,
+                "-u", userName,
+                "--port", port,
+                "-p",
+                "-n",           // <- This is important (https://dev.mysql.com/doc/refman/8.0/en/mysql-command-options.html#option_mysql_unbuffered)
+                "-L",           // <- This is important (https://dev.mysql.com/doc/refman/8.0/en/mysql-command-options.html#option_mysql_skip-line-numbers)
+                "-f",           // <- This is important (https://dev.mysql.com/doc/refman/8.0/en/mysql-command-options.html#option_mysql_force)
+                "-v",           // <- This is important (https://dev.mysql.com/doc/refman/8.0/en/mysql-command-options.html#option_mysql_verbose)
+                "-v",
+                "-v");
         try {
 
-            ProcessBuilder mysqlBuilder = new ProcessBuilder("mysql",
-                    "-h", host,
-                    "-u", userName,
-                    "--port", port,
-                    "-n",
-                    "-p",
-                    "-v");
+            mysqlBuilder.redirectErrorStream(true);
             this.mysql = mysqlBuilder.start();
+
+            processInputStream(mysql.getInputStream());
 
             this.mysql.getOutputStream().write((password + "\n").getBytes());
             this.mysql.getOutputStream().flush();
@@ -45,6 +56,7 @@ public class ShellFormController {
             if (mysql.isAlive()) {
                 mysql.destroyForcibly();
             }
+            Platform.exit();
         }
     }
 
@@ -54,16 +66,65 @@ public class ShellFormController {
         if (!txtCommand.getText().endsWith(";")) {
             statement += ";";
         }
+
         try {
+            txtOutput.clear();
+
+            if (statement.equalsIgnoreCase("exit;")){
+                Platform.exit();
+                return;
+            }
+
             this.mysql.getOutputStream().write((statement + "\n").getBytes());
             this.mysql.getOutputStream().flush();
+            txtCommand.selectAll();
 
-            InputStream is = this.mysql.getInputStream();
-            InputStream es = this.mysql.getErrorStream();
+            Pattern pattern = Pattern.compile(".*[;]?((?i)(use)) (?<db>[A-Za-z0-9-_]+);.*");
+            Matcher matcher = pattern.matcher(statement);
+            if (matcher.matches()){
+                lblCurrentSchema.setText("SCHEMA: " + matcher.group("db"));
+                txtOutput.setText("Database changed");
+                lblCurrentSchema.setTextFill(Paint.valueOf("blue"));
+            }
 
-            if (is.a)
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void processInputStream(InputStream is) {
+
+        Thread thread = new Thread(() -> {
+            try {
+                while (true) {
+                    byte[] buffer = new byte[1024];
+                    int read = is.read(buffer);
+
+                    if (read == -1) {
+                        break;
+                    }
+
+                    String output = new String(buffer, 0, read);
+                    Platform.runLater(() -> {
+                        txtOutput.appendText(output);
+
+                        /* A little hack */
+                        if (txtOutput.getText().contentEquals("Enter password: ")){
+                            txtOutput.clear();
+                            txtOutput.setText("Welcome to DEP-8 MySQL Client Shell\n" +
+                                              "----------------------------------------\n\n" +
+                                            "Please enter your command above to proceed.\nThank you! \uD83D\uDE09" +
+                                    "\n\nCopyright © 2022 DEP8 IJSE. All Rights Reserved.");
+                        }
+                    });
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        thread.setDaemon(true);
+        thread.start();
+
     }
 }
